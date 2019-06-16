fa7d52495209f8fb0f3a6bf73ea4f1ee9b2ef0be
August (Chris) <augu@voided.pw>
2019-06-16 14:20:05 -0700

    Start on the framework's typings

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,25 @@
-declare namespace Akashi {}
-export = Akashi;
+import Pikmin from 'pikmin';
+import Eris from 'eris';
+
+declare module '@yamashiro/akashi' {
+    export const version: string;
+    export class ArgumentParser {
+        constructor(raw: string[]);
+        public args: string[];
+        public gather(sep?: string): string;
+        public isEmpty(i: number): boolean;
+        public get(i: number): string;
+        public slice(arg: number, start?: number, end?: number): string;
+    }
+    export class Client extends Eris.Client {
+        constructor(options: DefaultOptions);
+        public manager: CommandManager;
+        public events: EventManager;
+        public database: DatabaseManager;
+        public rest: RESTClient;
+        public settings: GuildSettings;
+        public logger: Pikmin.instance;
+        public commandUsage: { [x: string]: CommandUsage };
+        public messagesSeen: number;
+    }
+}
